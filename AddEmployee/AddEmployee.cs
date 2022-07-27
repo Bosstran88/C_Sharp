98fca6d9913dbfda34119bfc0c2cb767c0e4d470
Bosstran88 <97713518+Bosstran88@users.noreply.github.com>
2022-07-27 22:27:53 +0700

    C#
    C#

---
+++ AddEmployee/AddEmployee.cs
@@ -0,0 +1,24 @@
+﻿namespace AddEmployee
+{
+    internal class AddEmployee
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Xin chao dai ca {0}",name);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Dai ka hay nhap address:");
+            string add = Console.ReadLine();
+            Console.WriteLine("Dai ka que "+add+" a");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("sdt cua dai ka la:");
+            string phone = Console.ReadLine();
+            Console.WriteLine(phone);
+
+        }   
+    }
+}
---
+++ Data Types/DataTypes.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Example
+{
+    static void Main(String[] args)
+    {
+        int intVal;
+        double doubleVal;
+        string strVal;
+        intVal = 10;
+        doubleVal = 3.142;
+        strVal = "Fpt Aptech";
+        Console.WriteLine("{0} is an integer values", intVal);
+        Console.WriteLine("{0} is an double values", doubleVal);
+        Console.WriteLine("{0} is an string", strVal);
+        Console.ReadLine();
+    }
+}
