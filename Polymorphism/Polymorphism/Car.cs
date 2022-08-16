5240af9651818860bb70bceefa08be18d988e864
Bosstran88 <97713518+Bosstran88@users.noreply.github.com>
2022-08-16 20:52:17 +0700

    Polymorphism
    polymorphism

---
+++ Polymorphism/Polymorphism/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Car
+{
+    public string make;
+    public string model;
+    public string color;
+    public int yearBuilt;
+
+    public void Start()
+    {
+        System.Console.WriteLine(model + " started");
+    }
+    public void Stop()
+    {
+        System.Console.WriteLine(model + " stopped");
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Car myCar;
+            // create a Car object , and assign adress to myCar
+            System.Console.WriteLine("Creating a Car object and assigning " +
+                " its memory location to myCar");
+            myCar = new Car();
+
+            // assign values to the Car object's field using mycar;
+            myCar.make = "Toyota";
+            myCar.model = " MR2";
+            myCar.color = "black";
+            myCar.yearBuilt = 1995;
+
+            //display the field values using myCar;
+            System.Console.WriteLine("mycar detail:");
+            System.Console.WriteLine("myCar.make = " + myCar.make);
+            System.Console.WriteLine("mycar.model = " + myCar.model);
+            System.Console.WriteLine("myCar.color = " + myCar.color);
+            System.Console.WriteLine("myCar.yearBuilt = " + myCar.yearBuilt);
+
+            // call the method using myCar
+            myCar.Start();
+            myCar.Stop();
+
+            //declare another Car object reference and
+            // create another Car object
+            System.Console.WriteLine("Create another Car object and" + "assign " +
+                "its memory location to redPorsche");
+            Car redPorsche = new Car();
+            redPorsche.make = "Porsche";
+            redPorsche.model = "Boxster";
+            redPorsche.color = "red";
+            redPorsche.yearBuilt = 2000;
+            System.Console.WriteLine("redPorsche is a " + redPorsche.model);
+            //change the object referenced by the myCar object 
+            //reference to the object referenced by redPorsche
+
+            System.Console.WriteLine("Assigning redPorsche to myCar");
+            myCar = redPorsche;
+            System.Console.WriteLine("myCar details:");
+            System.Console.WriteLine("myCar.make = " + myCar.make);
+            System.Console.WriteLine("myCar.model = " + myCar.model);
+            System.Console.WriteLine("myCar.color = " + myCar.color);
+            System.Console.WriteLine("myCar.yearBuilt = " + myCar.yearBuilt);
+
+            //assign null to myCar (myCar will no longer reference
+            //an object
+            myCar = null;
+            Console.ReadLine();
+
+
+        }
+    }
+}
