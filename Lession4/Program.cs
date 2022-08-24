f54d72f805ef815bb0d460d7ddae733e4992d31d
Bosstran88 <97713518+Bosstran88@users.noreply.github.com>
2022-08-24 08:44:46 +0700

    Exception
    Update

---
+++ Lession4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lession4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            byte result;
+            num1 = 30;
+            num2 = 60;
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0}*{1} = {2}", num1, num2, result);
+                Console.ReadLine();
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(" gia tri ngoai mien");
+            }
+        }
+    }
+}
+    
