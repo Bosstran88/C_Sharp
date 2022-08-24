70efbb4ffda792d2b61f34cb3634ecafbbc69b92
Bosstran88 <97713518+Bosstran88@users.noreply.github.com>
2022-08-24 07:58:07 +0700

    Exception
    Exception

---
+++ Lession2/StartEnd.cs
@@ -0,0 +1,39 @@
+﻿namespace Lession2
+{
+    internal class StartEnd
+    {
+        static void ReadNumber(int start, int end)
+        {
+
+            int[] allNumbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    allNumbers[i] = int.Parse(Console.ReadLine());
+                    if (allNumbers[i] < start || allNumbers[i] > end)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid format of input!");
+                    break;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("The number is out of the range!");
+                    break;
+                }
+                start = allNumbers[i]; // because in the tast is said that a1, a2, … a10, such that 1 < a1 < … < a10 < 100 !
+            }
+        }
+        static void Main()
+        {
+            int start = 1;
+            int end = 100;
+            ReadNumber(start, end);
+        }
+    }
+}
---
+++ Lession3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lession3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = new string[5];
+            weekdays[0] = " Sunday";
+            weekdays[1] = "Monday";
+            weekdays[2] = "Tuesday";
+            weekdays[3] = "wednesday";
+            weekdays[4] = "Thurday";
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(weekdays[i].ToString());
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
