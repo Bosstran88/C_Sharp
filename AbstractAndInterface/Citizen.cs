4fbc4e81acddb473b6373402b527c282847bb06b
Bosstran88 <97713518+Bosstran88@users.noreply.github.com>
2022-08-31 07:46:57 +0700

    AbstractAndInterface
    abstract

---
+++ AbstractAndInterface/Citizen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterface
+{
+    internal class Citizen : IPerson , IIdentifiable , IBirthable
+    {
+        public string Name{get;set;}
+        public int Age { get;set; }
+        public string Id { get;set; }
+        public string id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Birthdate { get;set; }
+
+        public string Birthable => throw new NotImplementedException();
+    }
+
+    internal interface IPerson
+    {
+    }
+}
