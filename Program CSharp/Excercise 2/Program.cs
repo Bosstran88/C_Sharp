64ab17a191f2d30f26e57fbd646eedb34e492b6d
Bosstran88 <97713518+Bosstran88@users.noreply.github.com>
2022-08-31 10:34:12 +0700

    Program CSharp
    Program

---
+++ Program CSharp/Excercise 2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Excercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+        }
+        abstract class Animal
+        {
+            public string Name { get; set; }
+
+            public double Weight { get; set; }
+            public Animal() { }
+            public Animal(string name, double weight)
+            {
+                Name = name;
+                Weight = weight;
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+            public static void Show()
+            {
+                Console.WriteLine("Name : {0}, Weight: {1}",)
+            }
+        }
+    }
+}
